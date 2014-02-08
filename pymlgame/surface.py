8bb7de65fc70452626d9e012ff3d34be2516fee5
XenGi <me@xengi.de>
2014-02-08 05:35:17 +0100

    massive improvements. fixes a very annoying bug in the matrix

--- pymlgame/surface.py
+++ pymlgame/surface.py
@@ -15,63 +15,60 @@ __status__ = "Development"
 
 import math
 
-from pymlgame.locals import *
+import pymlgame
 
 
 class Surface(object):
     """
     Represents a sheet to draw on
     """
-    def __init__(self, size):
+    def __init__(self, width, height):
         """
         Create a surface with default size and fill it with black pixels.
         """
-        self.width = size[0]
-        self.height = size[1]
-        self.matrix = []
-        self.fill(BLACK)
+        self.width = width
+        self.height = height
+        self.matrix = None
+        self.fill(pymlgame.BLACK)
 
     def fill(self, color, brightness=0.1):
         """
         Fill the whole screen with the given color.
         """
-        color = [val * brightness for val in color]
-        self.matrix = [[color for y in range(self.height)] for x in range(self.width)]
+        color = self.get_color(color, brightness)
+        self.matrix = [[color for _ in range(self.height)]
+                       for _ in range(self.width)]
 
     def draw_dot(self, pos, color, brightness=0.1):
         """
         Draw one single dot with the given color on the screen.
         """
-        color = [val * brightness for val in color]
+        realcolor = self.get_color(color, brightness)
         if 0 <= pos[0] < self.width and 0 <= pos[1] < self.height:
-            self.matrix[pos[0]][pos[1]] = color
-        else:
-            print('{:d},{:d} is outside the screen'.format(pos[0], pos[1]))
+            self.matrix[pos[0]][pos[1]] = realcolor
 
     def draw_line(self, start, end, color, brightness=0.1):
         """
         Draw a line with the given color on the screen.
         """
-        def dist(point, start, end):
-            return abs((end[0] - start[0]) * (start[1] - point[1]) -
-                       (start[0] - point[0]) * (end[1] - start[1])) / math.sqrt((end[0] - start[0])**2 +
-                                                                                (end[1] - start[1])**2)
+        def dist(p, a, b):
+            return abs((b[0] - a[0]) * (a[1] - p[1]) - (a[0] - p[0]) *
+                       (b[1] - a[1])) / math.sqrt((b[0] - a[0])**2 +
+                                                  (b[1] - a[1])**2)
 
-        color = [val * brightness for val in color]
         points = []
         for x in range(min(start[0], end[0]), max(start[0], end[0]) + 1):
             for y in range(min(start[1], end[1]), max(start[1], end[1]) + 1):
                 if dist((x, y), start, end) < 0.5:
                     points.append((x, y))
         for point in points:
-            self.draw_dot((point[0], point[1]), color)
+            self.draw_dot(point, color, brightness)
 
     def draw_rect(self, pos, size, color, fillcolor=None, brightness=0.1):
         """
         Draw a rectangle with the given color on the screen and optionally fill it with fillcolor.
         """
-        color = [val * brightness for val in color]
-        fillcolor = [val * brightness for val in fillcolor]
+        color = self.get_color(color, brightness)
         # draw top and botton line
         for x in range(size[0]):
             self.draw_dot((pos[0] + x, pos[1]), color)
@@ -81,7 +78,9 @@ class Surface(object):
             self.draw_dot((pos[0], pos[1] + y), color)
             self.draw_dot((pos[0] + size[0] - 1, pos[1] + y), color)
         # draw filled rect
+        #TODO: find out if the rect is at least 3x3 to actually have a filling
         if fillcolor:
+            fillcolor = self.get_color(fillcolor, brightness)
             for x in range(size[0] - 2):
                 for y in range(size[1] - 2):
                     self.draw_dot((pos[0] + 1 + x, pos[1] + 1 + y), fillcolor)
@@ -93,18 +92,26 @@ class Surface(object):
         def dist(point, pos):
             return abs(math.sqrt((pos[0] - point[0])**2 + (pos[1] - point[1])**2) - radius)
 
-        color = [val * brightness for val in color]
-        fillcolor = [val * brightness for val in fillcolor]
-        # draw outline
+        color = self.get_color(color, brightness)
         points = []
         for x in range(pos[0] - radius, pos[0] + radius):
             for y in range(pos[1] - radius, pos[1] + radius):
                 if 0 < x < self.width and 0 < y < self.height:
                     if dist((x, y), pos) < 1:
-                        self.draw_dot((x, y), color)
                         points.append((x, y))
 
         # draw fill color
         if fillcolor:
+            fillcolor = self.get_color(fillcolor, brightness)
             for point in points:
                 pass
+        # draw outline
+        for point in points:
+            self.draw_dot(point, color)
+
+    @staticmethod
+    def get_color(color, brightness=0.1):
+        """
+        Returns the color with applied brightness level
+        """
+        return tuple([math.floor(val * brightness) for val in color])
