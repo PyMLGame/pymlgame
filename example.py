a1d47e51540d80623d401d7774cca32c03da4bef
XenGi <me@xengi.de>
2014-02-08 07:08:26 +0100

    colors changed. example is now complete for the current feature set

--- example.py
+++ example.py
@@ -28,15 +28,24 @@ class Game(object):
         self.screen = pymlgame.Screen(host, port, width, height)
         self.clock = pymlgame.Clock()
         self.running = True
-        self.colors = [pymlgame.GREEN,
-                       pymlgame.WHITE,
-                       pymlgame.RED,
-                       pymlgame.YELLOW,
+        self.colors = [pymlgame.WHITE,
                        pymlgame.BLUE,
-                       pymlgame.CYAN]
+                       pymlgame.GREEN,
+                       pymlgame.CYAN,
+                       pymlgame.MAGENTA,
+                       pymlgame.YELLOW,
+                       pymlgame.RED]
 
         # surfaces
         self.corners = pymlgame.Surface(self.screen.width, self.screen.height)
+        self.lines = pymlgame.Surface(int(self.screen.width / 2) - 2,
+                                      int(self.screen.height / 2) - 2)
+        self.rects = pymlgame.Surface(int(self.screen.width / 2) - 2,
+                                      int(self.screen.height / 2) - 2)
+        self.circle = pymlgame.Surface(int(self.screen.width / 2) - 2,
+                                       int(self.screen.height / 2) - 2)
+        self.filled = pymlgame.Surface(int(self.screen.width / 2) - 2,
+                                       int(self.screen.height / 2) - 2)
 
     def update(self):
         """
@@ -52,42 +61,48 @@ class Game(object):
                               self.colors[0])
         self.corners.draw_dot((0, self.screen.height - 1), self.colors[0])
 
-        self.lines = pymlgame.Surface(38, 14)
-        lines.draw_line((0, 0), (37, 13), self.colors[1])
-        lines.draw_line((13, 0), (37, 0), self.colors[1])
+        self.lines.fill(pymlgame.BLACK)
+        self.lines.draw_line((1, 0), (self.lines.width - 1, 0), self.colors[1])
+        self.lines.draw_line((0, 1), (0, self.lines.height - 1), self.colors[3])
+        self.lines.draw_line((0, 0), (self.lines.width - 1,
+                                      self.lines.height - 1), self.colors[2])
 
-        rectangles = pymlgame.Surface(22, 10)
-        rectangles.draw_rect((0, 0), (10, 10), self.colors[2], self.colors[3])
-        rectangles.draw_rect((12, 0), (10, 10), self.colors[3], self.colors[2])
+        self.rects.fill(pymlgame.BLACK)
+        self.rects.draw_rect((0, 0), (int(self.rects.width / 2) - 1,
+                                      self.rects.height),
+                             self.colors[2], self.colors[3])
+        self.rects.draw_rect((int(self.rects.width / 2) + 1, 0),
+                             (int(self.rects.width / 2) - 1,
+                              self.rects.height),
+                             self.colors[3], self.colors[2])
 
-        circles = pymlgame.Surface(10, 10)
-        circles.draw_circle((4, 4), 4, self.colors[4], self.colors[5])
+        self.circle.fill(pymlgame.BLACK)
+        radius = int(min(self.circle.width, self.circle.height) / 2) - 1
+        self.circle.draw_circle((int(self.circle.width / 2) - 1,
+                                 int(self.circle.height / 2) - 1), radius,
+                                self.colors[4], self.colors[5])
+
+        self.filled.fill(self.colors[6])
 
     def render(self):
         """
         Send the current screen content to Mate Light
         """
-        self.screen.blit(self.bg)
+        self.screen.reset()
         self.screen.blit(self.corners)
         self.screen.blit(self.lines, (1, 1))
-        #self.screen.blit(rectangles, (3, 3))
-        #self.screen.blit(circles, (27, 3))
+        self.screen.blit(self.rects, (int(self.screen.width / 2) + 1, 1))
+        self.screen.blit(self.circle, (0, int(self.screen.height / 2) + 1))
+        self.screen.blit(self.filled, (int(self.screen.width / 2) + 1,
+                                       int(self.screen.height / 2) + 1))
 
         self.screen.update()
-        self.clock.tick(5)
+        self.clock.tick(2)
 
     def handle_events(self):
         """
-        Loop through all events and react to them
+        Loop through all events
         """
-        for event in pymlgame.events.get_events():
-            #print(event)
-            #if event['type'] == KEYUP:
-            #    if event['key'] == K_Arrow_Up or event['key'] == K_Arrow_Right:
-            #        self.colors.append(self.colors.pop(0))
-            #    elif event['key'] == K_Arrow_Down or event['key'] == K_Arrow_Left:
-            #        self.colors.insert(0, self.colors.pop())
-            pass
         self.colors.append(self.colors.pop(0))
 
     def gameloop(self):
@@ -95,7 +110,7 @@ class Game(object):
         A game loop that circles through the methods
         """
         try:
-            while self.running:
+            while True:
                 self.handle_events()
                 self.update()
                 self.render()
